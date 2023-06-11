fd0820f71a278f1c437c0cc1171ba264899a80aa
“zm <809739027@qq.com>
2023-06-11 10:59:18 +0800

    first commit

---
+++ src/test/java/testcase/Logintest.java
@@ -0,0 +1,11 @@
+package testcase;
+
+import org.testng.annotations.Test;
+
+public class Logintest {
+@Test
+        public void loginSucess(){
+        System.out.println("登录成功");
+}
+
+}
---
+++ src/test/java/testcase/Registertest.java
@@ -0,0 +1,10 @@
+package testcase;
+
+import org.testng.annotations.Test;
+
+public class Registertest {
+    @Test
+    public void registerSucess(){
+        System.out.println("注册成功");
+    }
+}
